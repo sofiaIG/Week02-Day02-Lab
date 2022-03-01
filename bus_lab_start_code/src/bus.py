7f80c41d58e12acb2e280b232667cb8eef92df30
user <user@users-MacBook-Pro.local>
2022-03-01 14:39:19 +0000

    Initial commit

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,8 @@
+class Bus:
+    def __init__(self, bus_number, destination):
+        self.bus_number = bus_number
+        self.destination = destination
+
+    def drive(self):
+        return("Brum brum")
+        
